acd9a5372b019fa1adec5ae89eb6fabdd87da73f
Alex <alex.podikoglou@gmail.com>
2026-01-01 01:09:44 +0200

    refactor: get rid of users table and nanoid

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,11 +1,4 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
-import { nanoid } from "nanoid";
-
-export const usersTable = sqliteTable("users", {
-	id: text().primaryKey().$defaultFn(nanoid),
-	username: text().notNull(),
-	password: text().notNull(),
-});
 
 export const entriesTable = sqliteTable("entries", {
 	id: integer().primaryKey({ autoIncrement: true }),
