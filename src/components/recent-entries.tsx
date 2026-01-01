cf916d493067e537bf915e54bd04f0fe6efb025c
Alex <alex.podikoglou@gmail.com>
2026-01-01 14:25:34 +0200

    perf: prepare recent entries query

--- src/components/recent-entries.tsx
+++ src/components/recent-entries.tsx
@@ -4,13 +4,15 @@ import { db } from "../db";
 import { entriesTable } from "../db/schema";
 import { EntriesTable } from "./entries-table";
 
+const recentEntriesQuery = db
+	.select()
+	.from(entriesTable)
+	.orderBy(desc(entriesTable.spottedOn))
+	.limit(20)
+	.prepare();
+
 export const RecentEntries: FC = async () => {
-	const entries = await db
-		.select()
-		.from(entriesTable)
-		.orderBy(desc(entriesTable.spottedOn))
-		.limit(20)
-		.execute();
+	const entries = await recentEntriesQuery.execute();
 
 	return <EntriesTable entries={entries} />;
 };
