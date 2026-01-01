395f1351d29fbc5584d566f06c697654cfd0376c
Alex <alex.podikoglou@gmail.com>
2026-01-01 04:18:09 +0200

    refactor: slightly improve design

--- src/pages/entry.tsx
+++ src/pages/entry.tsx
@@ -1,5 +1,6 @@
 import { format } from "date-fns";
 import { eq } from "drizzle-orm";
+import { css, cx } from "hono/css";
 import type { FC } from "hono/jsx";
 import { Layout } from "../components/layout";
 import { Navbar } from "../components/navbar";
@@ -27,6 +28,13 @@ export const EntryPage: FC<{ entryId: number }> = async ({ entryId }) => {
 
 	const entry = results[0]!; // NOTE: this sucks
 
+	const tdClass = css`
+    padding-right: 30px;
+  `;
+	const keyClass = css`
+    font-weight: bold;
+  `;
+
 	return (
 		<Layout>
 			<Navbar />
@@ -36,35 +44,35 @@ export const EntryPage: FC<{ entryId: number }> = async ({ entryId }) => {
 			<table>
 				<tbody>
 					<tr>
-						<td>Country Code</td>
-						<td>{entry.countryCode}</td>
+						<td class={cx(tdClass, keyClass)}>Country Code</td>
+						<td class={tdClass}>{entry.countryCode}</td>
 					</tr>
 
 					<tr>
-						<td>License Plate</td>
-						<td>{entry.text}</td>
+						<td class={cx(tdClass, keyClass)}>License Plate</td>
+						<td class={tdClass}>{entry.text}</td>
 					</tr>
 
 					<tr>
-						<td>EU</td>
-						<td>{entry.eu ? "Yes" : "No"}</td>
+						<td class={cx(tdClass, keyClass)}>EU</td>
+						<td class={tdClass}>{entry.eu ? "Yes" : "No"}</td>
 					</tr>
 
 					<tr>
-						<td>Notes</td>
-						<td>{entry.notes}</td>
+						<td class={cx(tdClass, keyClass)}>Notes</td>
+						<td class={tdClass}>{entry.notes}</td>
 					</tr>
 
 					<tr>
-						<td>Spotted On</td>
-						<td>
+						<td class={cx(tdClass, keyClass)}>Spotted On</td>
+						<td class={tdClass}>
 							{entry.spottedOn ? format(entry.spottedOn, "MMM yyyy") : "-"}
 						</td>
 					</tr>
 
 					<tr>
-						<td>Submitted</td>
-						<td>
+						<td class={cx(tdClass, keyClass)}>Submitted</td>
+						<td class={tdClass}>
 							{entry.createdAt ? format(entry.createdAt, "MMM yyyy") : "-"}
 						</td>
 					</tr>
