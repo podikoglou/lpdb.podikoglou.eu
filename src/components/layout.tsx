f971470934a0d6a60f1e2b8475fdd49244135499
Alex <alex.podikoglou@gmail.com>
2026-01-01 04:35:06 +0200

    feat: responsive

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,6 +5,7 @@ export const Layout: FC = (props) => {
 	return (
 		<html lang="en">
 			<head>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<link rel="stylesheet" href="/static/style.css" />
 				<link rel="stylesheet" href="/static/recursive.css" />
 				<Style />
